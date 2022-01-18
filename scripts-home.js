4c62dcaeb5d29da043ecd92c539eeedd5aa30467
Orlando Azuara <orlando_azu@MacBook-Pro.local>
2022-01-18 15:34:38 -0500

    fixed responsive bugs and added contact page

--- scripts-home.js
+++ scripts-home.js
@@ -13,3 +13,33 @@ function toggleMenu() {
         z.style.display = "none";
     }
 }
+
+function toggleForm1() {
+    var form = document.getElementById("form1");
+    var btn1 = document.getElementById("gen-b1");
+    var btn2 = document.getElementById("gen-b2");
+    if (form.style.display == "none") {
+        form.style.display = "flex";
+        btn1.style.display = "none";
+        btn2.style.display = "block";
+    } else {
+        form.style.display = "none";
+        btn1.style.display = "block";
+        btn2.style.display = "none";
+    }
+}
+
+function toggleForm2() {
+    var form = document.getElementById("form2");
+    var btn1 = document.getElementById("est-b1");
+    var btn2 = document.getElementById("est-b2");
+    if (form.style.display == "none") {
+        form.style.display = "flex";
+        btn1.style.display = "none";
+        btn2.style.display = "block";
+    } else {
+        form.style.display = "none";
+        btn1.style.display = "block";
+        btn2.style.display = "none";
+    }
+}
